edfc0837246e45cd15f02aec43e9d81aff582b39
unity3dexperience <artem@unity3dexperience.com>
2020-07-01 17:16:28 +0300

    Creating an architectory
    Creating class Gun:Weapons

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace unity3dexperience
+{
+    public sealed class Gun : Weapons
+    {
+        public override void Fire()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
